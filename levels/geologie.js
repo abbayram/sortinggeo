0de6787b543971de1fbe8a77374cac8e1adf0cb4
abbayram <94920176+abbayram@users.noreply.github.com>
2026-01-09 12:02:59 +0100

    Create geologie.js

---
+++ levels/geologie.js
@@ -0,0 +1,42 @@
+const levelData = [
+  { 
+    q: "8 Häufige Elemente in Erdkruste oder Kern", 
+    targets: ["Sauerstoff", "Silicium", "Aluminium", "Eisen", "Magnesium", "Calcium", "Nickel", "Schwefel"], 
+    count: 8 
+  },
+  { 
+    q: "7 Meilensteine der Erdgeschichte (Kalendermodell)", 
+    targets: ["Entstehung der Erde", "Entstehung des Mondes", "Erste Ozeane", "Erste Spuren von Leben", "Great Oxidation Event", "Vielzellige Organismen", "Kambrische Explosion"], 
+    count: 7 
+  },
+  { 
+    q: "6 Perioden des Paläozoikums (Erdaltertum)", 
+    targets: ["Kambrium", "Ordovizium", "Silur", "Devon", "Karbon", "Perm"], 
+    count: 6 
+  },
+  { 
+    q: "5 Komponenten des Klimasystems (Exogene Dynamik)", 
+    targets: ["Atmosphäre", "Hydrosphäre", "Kryosphäre", "Lithosphäre", "Sonnenenergie"], 
+    count: 5 
+  },
+  { 
+    q: "4 Schalen des Erdinneren", 
+    targets: ["Erdkruste", "Erdmantel", "Äußerer Kern", "Innerer Kern"], 
+    count: 4 
+  },
+  { 
+    q: "3 Evidenzen für die Theorie des Urknalls", 
+    targets: ["Hubble-Expansion", "Kosmische Hintergrundstrahlung", "Mengenverhältnisse leichter Elemente"], 
+    count: 3 
+  },
+  { 
+    q: "2 Ären des Phanerozoikums (vor der Neuzeit)", 
+    targets: ["Paläozoikum", "Mesozoikum"], 
+    count: 2 
+  },
+  { 
+    q: "1 Wissenschaft von der zeitlichen Gesteinsfolge", 
+    targets: ["Stratigraphie"], 
+    count: 1 
+  }
+];
