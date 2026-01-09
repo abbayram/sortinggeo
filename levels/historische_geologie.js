779d7abe822c17d5d096af91fa3bfb8df2fefa65
abbayram <94920176+abbayram@users.noreply.github.com>
2026-01-09 12:13:03 +0100

    Create historische_geologie.js

---
+++ levels/historische_geologie.js
@@ -0,0 +1,42 @@
+const levelData = [
+  { 
+    q: "8 Geologische Perioden des Phanerozoikums", 
+    targets: ["Kambrium", "Ordovizium", "Silur", "Devon", "Karbon", "Perm", "Trias", "Jura"], 
+    count: 8 
+  },
+  { 
+    q: "7 Epochen des Känozoikums (Erdneuzeit)", 
+    targets: ["Paläozän", "Eozän", "Oligozän", "Miozän", "Pliozän", "Pleistozän", "Holozän"], 
+    count: 7 
+  },
+  { 
+    q: "6 Biologische Proxies der Paläoklimatologie", 
+    targets: ["Pollen", "Insekten", "Korallen", "Diatomeen", "Ostrakoden", "Makrofossilien"], 
+    count: 6 
+  },
+  { 
+    q: "5 Wesentliche Proxy-Archive", 
+    targets: ["Eisbohrkerne", "Ozeansedimente", "Seesedimente", "Baumringe", "Speläotheme"], 
+    count: 5 
+  },
+  { 
+    q: "4 Fehlerquellen bei der Lumineszenz-Datierung", 
+    targets: ["Sättigung", "Ungenügende Bleichung", "Strahlungsdosis-Fehler", "Wassergehalt-Einfluss"], 
+    count: 4 
+  },
+  { 
+    q: "3 Kernbereiche der Paläoklimatologie", 
+    targets: ["Datierung", "Proxies", "Modellierung"], 
+    count: 3 
+  },
+  { 
+    q: "2 Isotope der Sauerstoffmethode", 
+    targets: ["16O", "18O"], 
+    count: 2 
+  },
+  { 
+    q: "1 Forschungsfeld für Klimate vor instrumentellen Messungen", 
+    targets: ["Paläoklimatologie"], 
+    count: 1 
+  }
+];
