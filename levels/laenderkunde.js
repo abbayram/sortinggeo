df609b90ac9c22afee19de8f8a948a7497d6d02a
abbayram <94920176+abbayram@users.noreply.github.com>
2026-01-09 12:19:53 +0100

    Create laenderkunde.js

---
+++ levels/laenderkunde.js
@@ -0,0 +1,42 @@
+const levelData = [
+  { 
+    q: "8 Teilbereiche der Klassischen Geographie", 
+    targets: ["Physische Geographie", "Anthropogeographie", "Landschaftsgeographie", "Länderkunde", "Hydrosphäre", "Klimatologie", "Bevölkerungsgeographie", "Wirtschaftsgeographie"], 
+    count: 8 
+  },
+  { 
+    q: "7 Bestandteile eines Länderkunde-Schemas (Länderkunde-Kanon)", 
+    targets: ["Muttergestein", "Relief", "Klima", "Pflanzenwelt", "Tierwelt", "Bevölkerung", "Wirtschaft"], 
+    count: 7 
+  },
+  { 
+    q: "6 Teildisziplinen der Physischen Geographie", 
+    targets: ["Hydrosphäre", "Pedosphäre", "Klimatologie", "Biogeographie", "Geomorphologie", "Geologie"], 
+    count: 6 
+  },
+  { 
+    q: "5 Funktionen wissenschaftlicher Theorien (nach Hard)", 
+    targets: ["Hypothesen erzeugen", "Forschungsprogramme entwickeln", "Daten produzieren", "Theorien ausdifferenzieren", "Theorien widerlegen"], 
+    count: 5 
+  },
+  { 
+    q: "4 Pioniere der wissenschaftlichen Geographie", 
+    targets: ["Alexander von Humboldt", "Carl Ritter", "Friedrich Ratzel", "Alfred Hettner"], 
+    count: 4 
+  },
+  { 
+    q: "3 Fokusbereiche der ökonomischen DDR-Länderkunde", 
+    targets: ["Chemische Industrie", "Metallurgie", "Maschinenbau"], 
+    count: 3 
+  },
+  { 
+    q: "2 Grundzweige im System der Geographie", 
+    targets: ["Allgemeine Geographie", "Regionale Geographie"], 
+    count: 2 
+  },
+  { 
+    q: "1 Kerngebiet der klassischen Geographie", 
+    targets: ["Länderkunde"], 
+    count: 1 
+  }
+];
