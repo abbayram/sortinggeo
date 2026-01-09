ee824bfc796043d1b2dd9995a3631fd006173909
abbayram <94920176+abbayram@users.noreply.github.com>
2026-01-09 12:14:02 +0100

    Create klimatologie_dynamik.js

---
+++ levels/klimatologie_dynamik.js
@@ -0,0 +1,42 @@
+const levelData = [
+  { 
+    q: "8 Kleinräumige Zirkulationssysteme & Komponenten", 
+    targets: ["Landwind", "Seewind", "Bergwind", "Talwind", "Hangaufwind", "Hangabwind", "Flurwind", "Stadt-Umland-Wind"], 
+    count: 8 
+  },
+  { 
+    q: "7 Faktoren und Effekte der Corioliskraft", 
+    targets: ["Erdrotation", "Geographische Breite", "Windgeschwindigkeit", "Ablenkung NH-Rechts", "Ablenkung SH-Links", "Breitenabhängigkeit", "Scheinkraft"], 
+    count: 7 
+  },
+  { 
+    q: "6 Großräumige Strömungstypen & Druckgebilde", 
+    targets: ["Geostrophischer Wind", "Zyklostrophischer Wind", "Reibungswind", "Gradientwind", "Zyklone", "Antizyklone"], 
+    count: 6 
+  },
+  { 
+    q: "5 Ursachen für vertikale Luftbewegungen", 
+    targets: ["Orographische Hebung", "Thermische Hebung", "Frontale Hebung", "Konvergenz", "Dichteunterschiede"], 
+    count: 5 
+  },
+  { 
+    q: "4 Grundbegriffe des Luftdruckfeldes", 
+    targets: ["Hochdruckgebiet", "Tiefdruckgebiet", "Isobaren", "Druckgradientkraft"], 
+    count: 4 
+  },
+  { 
+    q: "3 Schichtungszustände der Atmosphäre", 
+    targets: ["Stabil", "Labil", "Indifferent"], 
+    count: 3 
+  },
+  { 
+    q: "2 Fundamentale adiabatische Gradienten", 
+    targets: ["Trockenadiabatisch", "Feuchtadiabatisch"], 
+    count: 2 
+  },
+  { 
+    q: "1 Zentraler Gleichgewichtszustand der freien Atmosphäre", 
+    targets: ["Geostrophisches Gleichgewicht"], 
+    count: 1 
+  }
+];
