b1e17d66d2ac9d9ead9a628de145891d7854c3f4
abbayram <94920176+abbayram@users.noreply.github.com>
2026-01-09 12:13:23 +0100

    Create klimatologie.js

---
+++ levels/klimatologie.js
@@ -0,0 +1,42 @@
+const levelData = [
+  { 
+    q: "8 Zentrale Klimafaktoren und Bestimmungsgrößen", 
+    targets: ["Geographische Breite", "Meereshöhe", "Land-Meer-Verteilung", "Meeresströmungen", "Relief", "Exposition", "Bodenbedeckung", "Albedo"], 
+    count: 8 
+  },
+  { 
+    q: "7 Komponenten der atmosphärischen Strahlungsinteraktion", 
+    targets: ["Streuung", "Reflexion", "Absorption", "Emission", "Transmission", "Rückstrahlung", "Gegenstrahlung"], 
+    count: 7 
+  },
+  { 
+    q: "6 Themenfelder der Klimatologie-Einführung", 
+    targets: ["Strahlungsbilanz", "Energiebilanz", "Wasserhaushalt", "Zirkulationsgrundlagen", "Allgemeine Zirkulation", "Klimazonen"], 
+    count: 6 
+  },
+  { 
+    q: "5 Signifikante Treibhausgase der Atmosphäre", 
+    targets: ["Wasserdampf", "Kohlendioxid", "Methan", "Distickoxid", "Ozon"], 
+    count: 5 
+  },
+  { 
+    q: "4 Stoffe zur Bestimmung der spezifischen Wärmekapazität", 
+    targets: ["Luft", "Sand", "Organische Substanz", "Wasser"], 
+    count: 4 
+  },
+  { 
+    q: "3 Elementare Strahlungsgesetze", 
+    targets: ["Plancksches Gesetz", "Stefan-Boltzmann-Gesetz", "Wiensches Gesetz"], 
+    count: 3 
+  },
+  { 
+    q: "2 Physikalische Streumechanismen", 
+    targets: ["Rayleigh-Streuung", "Mie-Streuung"], 
+    count: 2 
+  },
+  { 
+    q: "1 Kennwert der solaren Einstrahlung (Atmosphärenobergrenze)", 
+    targets: ["Solarkonstante"], 
+    count: 1 
+  }
+];
