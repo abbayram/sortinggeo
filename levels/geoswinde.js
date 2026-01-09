fa0b57a5ea3b9918f6b9c9f62ddcc1e9e6ffb351
abbayram <94920176+abbayram@users.noreply.github.com>
2026-01-09 03:19:44 +0100

    Create geoswinde.js

---
+++ levels/geoswinde.js
@@ -0,0 +1,42 @@
+const levelData = [
+  { 
+    q: "8 Atmosphärische Zirkulationszellen & Windsysteme", 
+    targets: ["Hadley-Zelle", "Ferrel-Zelle", "Walker-Zirkulation", "Passatwinde", "Westwindzone", "Polarwirbel", "Jetstream", "Brewer-Dobson-Zirkulation"], 
+    count: 8 
+  },
+  { 
+    q: "7 Wetterstation-Sensoren", 
+    targets: ["Anemometer", "Hygrometer", "Thermometer", "Pyranometer", "Barometer", "Pluviometer", "Datenlogger"], 
+    count: 7 
+  },
+  { 
+    q: "6 Physikalische Kräfte & Effekte", 
+    targets: ["Corioliskraft", "Zentrifugalkraft", "Druckgradientkraft", "Reibungskraft", "Gravitation", "Beta-Effekt"], 
+    count: 6 
+  },
+  { 
+    q: "5 Wellenphänomene", 
+    targets: ["Rossby-Wellen", "Schwerewellen", "Poincaré-Wellen", "Kelvin-Wellen", "akustische Wellen"], 
+    count: 5 
+  },
+  { 
+    q: "4 Wichtige Zustandsgrößen", 
+    targets: ["Potenzielle Temperatur", "Spezifische Feuchte", "Auftrieb", "Dichte"], 
+    count: 4 
+  },
+  { 
+    q: "3 Kleinräumige Windsysteme", 
+    targets: ["Land-See-Wind", "Hangwind", "Föhn"], 
+    count: 3 
+  },
+  { 
+    q: "2 Mathematische Ansätze & Modelle", 
+    targets: ["Quasi-geostrophische Theorie", "Flachwassergleichungen"], 
+    count: 2 
+  },
+  { 
+    q: "1 Zentrales Gleichgewichtskonzept", 
+    targets: ["Geostrophisches Gleichgewicht"], 
+    count: 1 
+  }
+];
