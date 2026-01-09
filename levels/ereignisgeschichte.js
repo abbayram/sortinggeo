1469c9b555c208891162567c51ff0bec79abbc7d
abbayram <94920176+abbayram@users.noreply.github.com>
2026-01-09 03:39:46 +0100

    Create ereignisgeschichte.js

---
+++ levels/ereignisgeschichte.js
@@ -0,0 +1,42 @@
+const levelData = [
+  { 
+    q: "8 Deutsche Könige des Spätmittelalters", 
+    targets: ["Rudolf I. von Habsburg", "Adolf von Nassau", "Albrecht I. von Habsburg", "Heinrich VII. von Luxemburg", "Ludwig IV. der Bayer", "Karl IV. von Luxemburg", "Wenzel von Luxemburg", "Ruprecht von der Pfalz"], 
+    count: 8 
+  },
+  { 
+    q: "7 Reichsstände und Wahlgremium", 
+    targets: ["Erzbischöfe", "Bischöfe", "Reichsäbte", "Reichsäbtissinnen", "weltliche Fürsten", "Reichsstädte", "Kurfürsten"], 
+    count: 7 
+  },
+  { 
+    q: "6 Zentrale Territorien im Reich", 
+    targets: ["Schwaben", "Bayern", "Franken", "Sachsen", "Rheinpfalz", "Elsass"], 
+    count: 6 
+  },
+  { 
+    q: "5 Ursachen des 100-jährigen Krieges", 
+    targets: ["Lehensrecht", "Dynastisches Erbrecht", "Allianz England-Schottland", "Wirtschaftliche Interessen", "Konfiskation der Festlands-Lehen"], 
+    count: 5 
+  },
+  { 
+    q: "4 Klassische Hofämter", 
+    targets: ["Schenk", "Truchsess", "Kämmerer", "Marschall"], 
+    count: 4 
+  },
+  { 
+    q: "3 Meilensteine der Reichsreform", 
+    targets: ["Reichslandfrieden 1442", "Kammergerichtsordnung 1471", "Reichskammergericht 1495"], 
+    count: 3 
+  },
+  { 
+    q: "2 Dynastien in Frankreich", 
+    targets: ["Kapetinger", "Valois"], 
+    count: 2 
+  },
+  { 
+    q: "1 Zentrales Rechtsdokument des Reiches", 
+    targets: ["Goldene Bulle von 1356"], 
+    count: 1 
+  }
+];
